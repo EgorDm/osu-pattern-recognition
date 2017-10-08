0714b8433082e3f620b9bc353fffbc6336f7eb04
Egor Dmitriev <egordmitriev2@gmail.com>
2017-10-08 21:47:37 +0200

    Added circumcenter calculation

--- math.cpp
+++ math.cpp
@@ -19,5 +19,34 @@ namespace math {
 
         return fabsf(atan2f(cross, dot));
     }
+
+    osupp::Coordinate pcentroid(osupp::Coordinate c0, osupp::Coordinate c1, osupp::Coordinate c2) {
+        return (c0 + c1 + c2) / 3.0;
+    }
+
+
+    //https://github.com/robu3/delaunay/blob/master/Triangle.java
+    osupp::Coordinate pcircumcenter(osupp::Coordinate a, osupp::Coordinate b, osupp::Coordinate c) {
+        auto midAB = midpoint(a, b), midBC = midpoint(b, c);
+        auto slopeAB = -1 / slope(a, b), slopeBC = -1 / slope(b, c);
+
+        auto bAB = midAB.y - slopeAB * midAB.x;
+        auto bBC = midBC.y - slopeBC * midBC.x;
+
+        auto slopeTot = slopeBC - slopeAB;
+        if(slopeTot == 0) return {std::numeric_limits<float>::quiet_NaN(), std::numeric_limits<float>::quiet_NaN()};
+        float x = (bAB - bBC) / slopeTot;
+
+        return {x, (slopeAB * x) + bAB};
+    }
+
+    osupp::Coordinate midpoint(osupp::Coordinate a, osupp::Coordinate b) {
+        return {(a.x + b.x) / 2, (a.y + b.y) / 2};
+    }
+
+    float slope(osupp::Coordinate a, osupp::Coordinate b) {
+        return {(b.y - a.y) / (b.x - a.x)};
+    }
+
 }
 
--- math.h
+++ math.h
@@ -12,6 +12,8 @@
 namespace math {
     float pangle(osupp::Coordinate c0, osupp::Coordinate c1, osupp::Coordinate c2);
 
+    osupp::Coordinate pcentroid(osupp::Coordinate c0, osupp::Coordinate c1, osupp::Coordinate c2);
+
     inline float raddeg(float rad) {
         return static_cast<float>(rad * 180 / M_PI);
     }
@@ -19,6 +21,13 @@ namespace math {
     inline float degrad(float deg) {
         return static_cast<float>(deg / 180 * M_PI);
     }
+
+    osupp::Coordinate pcircumcenter(osupp::Coordinate a, osupp::Coordinate b, osupp::Coordinate c);
+
+    osupp::Coordinate midpoint(osupp::Coordinate a, osupp::Coordinate b);
+
+    float slope(osupp::Coordinate a, osupp::Coordinate b);
+
 }
 
 #endif //OSU_PATTERN_MATH_H
