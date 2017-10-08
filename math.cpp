b966598664c67b46114559279e6d4fb4000358ae
Egor Dmitriev <egordmitriev2@gmail.com>
2017-10-08 21:44:23 +0200

    Added math namespace for functions

---
+++ math.cpp
@@ -0,0 +1,14 @@
+//
+// Created by egordm on 2-10-2017.
+//
+
+#include <iostream>
+#include <limits>
+#include "math.h"
+
+using namespace utils;
+
+namespace math {
+
+}
+
---
+++ math.h
@@ -0,0 +1,15 @@
+//
+// Created by egordm on 2-10-2017.
+//
+
+#ifndef OSU_PATTERN_MATH_H
+#define OSU_PATTERN_MATH_H
+
+
+#include <maths.h>
+#include <math.h>
+
+namespace math {
+}
+
+#endif //OSU_PATTERN_MATH_H
