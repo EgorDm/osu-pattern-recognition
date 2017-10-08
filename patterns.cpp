f60797e18320a84d28d37022ac32a4b836c682d9
Egor Dmitriev <egordmitriev2@gmail.com>
2017-10-08 21:43:23 +0200

    Added base pattern builder

--- patterns.cpp
+++ patterns.cpp
@@ -3,3 +3,27 @@
 //
 
 #include "patterns.h"
+
+
+bool PatternBuilder::step(std::shared_ptr<osupp::HitObject> obj) {
+    return obj != nullptr;
+}
+
+bool PatternBuilder::validate() {
+    return true;
+}
+
+Pattern *PatternBuilder::build(BeatmapIterator &iterator) {
+    unsigned int offset = 0;
+    objects.clear();
+    while (true) {
+        auto obj = iterator.getHitObject(offset);
+        if (!step(obj)) break;
+        offset++;
+    }
+    if (objects.size() < min_objects()) return nullptr;
+    if (!validate()) return nullptr;
+    iterator.obj_cursor += offset;
+
+    return instantiate();
+}
--- patterns.h
+++ patterns.h
@@ -18,6 +18,22 @@ public:
     unsigned long time{};
 };
 
+class PatternBuilder { //TODO: explode && use modular checks(add classifiers)
+public:
+    explicit PatternBuilder() = default;
+
+    virtual Pattern *build(BeatmapIterator &iterator);
+protected:
+    virtual bool step(std::shared_ptr<osupp::HitObject> obj);
+
+    virtual bool validate();
+
+    virtual Pattern *instantiate() = 0;
+
+    virtual unsigned int min_objects() = 0;
+
+    std::vector<std::shared_ptr<osupp::HitObject>> objects;
+};
 
 
 
